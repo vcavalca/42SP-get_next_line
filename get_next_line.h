cb785e9c81aaf86d0aa5cb476044c7b26c4282a3
vcavalca <61808793+vcavalca@users.noreply.github.com>
2021-06-01 16:20:25 -0300

    Add libs and fiz reader

--- get_next_line.h
+++ get_next_line.h
@@ -6,11 +6,15 @@
 /*   By: vcavalca <vcavalca@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/31 17:38:15 by vcavalca          #+#    #+#             */
-/*   Updated: 2021/05/31 17:42:43 by vcavalca         ###   ########.fr       */
+/*   Updated: 2021/06/01 16:19:03 by vcavalca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
 
 int	get_next_line(int fd, char **line);
 
