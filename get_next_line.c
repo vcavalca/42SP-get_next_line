3560a3c57919d06e5af4535a175249524e7de61a
vcavalca <61808793+vcavalca@users.noreply.github.com>
2021-06-01 17:19:10 -0300

    Update

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: vcavalca <vcavalca@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/31 17:37:38 by vcavalca          #+#    #+#             */
-/*   Updated: 2021/05/31 17:43:53 by vcavalca         ###   ########.fr       */
+/*   Updated: 2021/06/01 17:18:48 by vcavalca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,8 @@
 
 int	get_next_line(int fd, char **line)
 {
-	
+	int	return_ft;
+
+	if (fd < 0 || !line)
+		return (-1);
 }
