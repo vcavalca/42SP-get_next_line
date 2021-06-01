ad4625ea8e234103bc149ecde070107056642aa0
vcavalca <61808793+vcavalca@users.noreply.github.com>
2021-06-01 16:56:20 -0300

    Add prototype ft_strlcat

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: vcavalca <vcavalca@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/31 17:38:15 by vcavalca          #+#    #+#             */
-/*   Updated: 2021/06/01 16:40:13 by vcavalca         ###   ########.fr       */
+/*   Updated: 2021/06/01 16:55:54 by vcavalca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,6 @@ int		get_next_line(int fd, char **line);
 
 size_t	ft_strlen(const char *s);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 
 #endif
