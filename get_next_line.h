8ca9508850f01ad3b8e71f7593f9fc05ecc1e1fd
vcavalca <61808793+vcavalca@users.noreply.github.com>
2021-06-01 17:02:49 -0300

    Add prototype ft_strjoin

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: vcavalca <vcavalca@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/31 17:38:15 by vcavalca          #+#    #+#             */
-/*   Updated: 2021/06/01 16:55:54 by vcavalca         ###   ########.fr       */
+/*   Updated: 2021/06/01 17:01:40 by vcavalca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,5 +21,7 @@ int		get_next_line(int fd, char **line);
 size_t	ft_strlen(const char *s);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
+
+char	*ft_strjoin(char const *s1, char const *s2);
 
 #endif
