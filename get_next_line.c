6b97e0725a792f828bc0f9d3197c82c1d270f70c
vcavalca <61808793+vcavalca@users.noreply.github.com>
2021-06-02 13:35:47 -0300

    Update

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: vcavalca <vcavalca@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/31 17:37:38 by vcavalca          #+#    #+#             */
-/*   Updated: 2021/06/02 13:20:53 by vcavalca         ###   ########.fr       */
+/*   Updated: 2021/06/02 13:34:40 by vcavalca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,16 @@
 
 int	get_next_line(int fd, char **line)
 {
-	char	buf[BUFFER_SIZE + 1];
-	int		size;
+	static char	*new_line[POSITION];
+	char		buf[BUFFER_SIZE + 1];
+	int			size;
 
-	if (fd < 0 || (line == 0) || BUFFER_SIZE <= 0)
+	if (fd < 0 || line == 0 || BUFFER_SIZE <= 0)
 		return (-1);
 	size = read(fd, buf, BUFFER_SIZE);
 	while (size > 0)
 	{
 		buf[size] = '\0';
+		new_line[fd] = ft_strjoin(new_line[fd], buf);
 	}
 }
