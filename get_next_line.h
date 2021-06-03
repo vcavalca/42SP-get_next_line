d6afd4746b0b50b824e967afeb3b3d116dd0e79f
vcavalca <61808793+vcavalca@users.noreply.github.com>
2021-06-03 12:15:18 -0300

    Add prototype ft_memmove

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: vcavalca <vcavalca@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/31 17:38:15 by vcavalca          #+#    #+#             */
-/*   Updated: 2021/06/02 17:04:18 by vcavalca         ###   ########.fr       */
+/*   Updated: 2021/06/03 12:07:09 by vcavalca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,5 +21,7 @@ int		get_next_line(int fd, char **line);
 size_t	ft_strlen(const char *s);
 
 char	*ft_strjoin(char const *s1, char const *s2);
+
+void	*ft_memmove(void *dst, const void *src, size_t len);
 
 #endif
