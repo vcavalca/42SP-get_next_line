779a31f338ae46b1b85635fd945d452196cc1d98
vcavalca <61808793+vcavalca@users.noreply.github.com>
2021-06-05 11:03:51 -0300

    Add prototype

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: vcavalca <vcavalca@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/31 17:38:15 by vcavalca          #+#    #+#             */
-/*   Updated: 2021/06/05 10:48:42 by vcavalca         ###   ########.fr       */
+/*   Updated: 2021/06/05 11:03:24 by vcavalca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,11 @@
 
 # include <unistd.h>
 # include <stdlib.h>
+
+size_t	ft_strlen(const char *s);
+
+char	*ft_strmalloc(size_t size);
+
+char	*ft_strjoin(const char *s1, const char *s2);
 
 #endif
