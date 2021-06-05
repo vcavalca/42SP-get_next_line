7eb7b2b433f30c37c9a4a70f7fbbda8e56726621
vcavalca <61808793+vcavalca@users.noreply.github.com>
2021-06-05 11:02:08 -0300

    Add ft_strjoin from libft

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: vcavalca <vcavalca@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/31 17:38:18 by vcavalca          #+#    #+#             */
-/*   Updated: 2021/06/05 10:56:20 by vcavalca         ###   ########.fr       */
+/*   Updated: 2021/06/05 11:01:30 by vcavalca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,4 +32,29 @@ char	*ft_strmalloc(size_t size)
 	if (c == NULL)
 		return (NULL);
 	return (c);
+}
+
+char	*ft_strjoin(const char *s1, const char *s2)
+{
+	char	*new_s;
+	int		i;
+	int		j;
+
+	new_s = (char *)ft_strmalloc((ft_strlen(s1) + ft_strlen(s2)));
+	if (new_s == NULL)
+		return (NULL);
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		new_s[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[j] != '\0')
+	{
+		new_s[i + j] = s2[j];
+		j++;
+	}
+	new_s[i + j] = '\0';
+	return (new_s);
 }
