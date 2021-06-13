c853987e9d3a73ca37b172f5261a90b96c7dee7c
vcavalca <61808793+vcavalca@users.noreply.github.com>
2021-06-13 16:14:26 -0300

    Update

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -6,13 +6,13 @@
 /*   By: vcavalca <vcavalca@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/31 17:37:38 by vcavalca          #+#    #+#             */
-/*   Updated: 2021/06/05 12:29:37 by vcavalca         ###   ########.fr       */
+/*   Updated: 2021/06/13 16:13:48 by vcavalca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line_bonus.h"
 
-int	get_next_line_return(char *s)
+int	ft_gnl_is_in(char *s)
 {
 	int	i;
 
@@ -28,7 +28,7 @@ int	get_next_line_return(char *s)
 	return (0);
 }
 
-char	*get_next_line_save(char *s)
+char	*ft_gnl_save(char *s)
 {
 	char	*c;
 	int		i;
@@ -56,7 +56,7 @@ char	*get_next_line_save(char *s)
 	return (c);
 }
 
-char	*get_nxt_line(char *s)
+char	*ft_gnl_set_line(char *s)
 {
 	size_t		i;
 	char		*c;
@@ -88,7 +88,7 @@ int	get_next_line(int fd, char **line)
 	i = 1;
 	if (fd < 0 || !line || BUFFER_SIZE <= 0)
 		return (-1);
-	while (!get_next_line_return(hold) && i != 0)
+	while (!ft_gnl_is_in(hold) && i != 0)
 	{
 		i = read(fd, buf, BUFFER_SIZE);
 		if (i == -1)
@@ -96,8 +96,8 @@ int	get_next_line(int fd, char **line)
 		buf[i] = '\0';
 		hold = ft_strjoin(hold, buf);
 	}
-	*line = get_nxt_line(hold);
-	hold = get_next_line_save(hold);
+	*line = ft_gnl_set_line(hold);
+	hold = ft_gnl_save(hold);
 	if (i == 0)
 		return (0);
 	return (1);
